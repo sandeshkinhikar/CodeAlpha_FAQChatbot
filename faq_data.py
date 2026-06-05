53953b8f9e60277a2831340cdb1eb010a6e3bb4c
sandeshkinhikar <sandeshkinhikar8127@gmail.com>
2026-06-05 20:47:46 +0530

    Create  faq_data.py

---
+++ faq_data.py
@@ -0,0 +1,25 @@
+faq_data = {
+    "What is Python?":
+        "Python is a high-level programming language.",
+
+    "What is Artificial Intelligence?":
+        "Artificial Intelligence is the simulation of human intelligence by machines.",
+
+    "What is Machine Learning?":
+        "Machine Learning is a subset of AI that learns from data.",
+
+    "What is Data Science?":
+        "Data Science involves extracting insights from data.",
+
+    "What is Tkinter?":
+        "Tkinter is a GUI library in Python.",
+
+    "What is CodeAlpha?":
+        "CodeAlpha provides internships and training opportunities.",
+
+    "Who developed Python?":
+        "Python was developed by Guido van Rossum.",
+
+    "What is NLP?":
+        "NLP stands for Natural Language Processing."
+}
